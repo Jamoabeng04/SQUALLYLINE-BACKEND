c63f1621fd1346251163630feab46695ead9d60a
Jamoabeng04 <johnamoabeng04@gmail.com>
2026-08-25 00:57:26 +0000

    production setup

--- squallyline/settings.py
+++ squallyline/settings.py
@@ -41,7 +41,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://172.20.10.4:8001',
     'http://172.20.10.4:3000',
     'http://172.20.10.4:3001',
-    'https://squallyline-api.up.railway.app/',
+    'https://squallyline-api.up.railway.app',
     'https://squallyline-frontend-production.up.railway.app',
 ]
 
